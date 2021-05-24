077032f4b92c2df8276346e700b4a8fa4c669c79
WeXLer67 <79367408+WeXLer67@users.noreply.github.com>
2021-05-25 01:15:13 +0300

    Delete lemings directory

--- lemings/definitions.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# Константа для корневой дериктории
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-
-# Этот цвет для персонажей считается зоной свободного передвижения
-BACKGROUND_COLOUR = (114,114,201,255)
--- lemings/utils/getPathToFileFromFolder.py
+++
@@ -1,6 +0,0 @@
-import os
-from definitions import ROOT_DIR
-
-# Функция для удобного импорта файлов
-def getPathToFileFromFolder(filePath):
-    return os.path.join(ROOT_DIR, filePath)
